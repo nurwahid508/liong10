e233a747ed01b749a18cb877c799e6c96467320e
nurwahid508 <80597744+nurwahid508@users.noreply.github.com>
2021-03-16 10:44:24 +0800

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,23 @@
+<?php
+
+// 1 Masukan Semuah data awal login && Misi2 nya
+// Simak video dengan baik karna apk ini bnyak data2 nya
+
+
+$Excgd="xxxxxxxx";
+$token="xxxxxxxx";
+$Useragent="xxxxxx";
+
+// data id balance list 
+
+$id="xxxxxxxx";
+
+// data daily read 
+
+$Manifest="xxxxxxx";
+
+// data open peti 
+
+$petih="xxxxxxx";
+
+
